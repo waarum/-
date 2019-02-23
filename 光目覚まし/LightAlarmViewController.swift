f8f27cec80daded6bde8a4b271af1af3d0ee4e27
Waarum <merawataru@meryouwatarunomacbook-air.local>
2019-02-23 11:07:15 +0900

    Rename the Light Alarm  View Controller

--- 光目覚まし/LightAlarmViewController.swift
+++ 光目覚まし/LightAlarmViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class LightAlarmViewController: UIViewController {
 
     @IBOutlet weak var lightSlider: UISlider!
     
