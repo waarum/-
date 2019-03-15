79c4d2376455c358d344e3043d7bfd4519eaddf4
Waarum <merawataru@meryouwatarunomacbook-air.local>
2019-03-15 19:45:15 +0900

    Complete the wake up functions

---
+++ 光目覚まし/Controllers/AlarmSetViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  光目覚まし
+//
+//  Created by 目良渉 on 2019/02/11.
+//  Copyright © 2019 Wataru Mera. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Foundation
+
+class AlarmSetViewController: UIViewController {
+    
+    let userDefaults = UserDefaults.standard
+    
+    let lightIntervals = [1,2,3,4,5,6,7,8,9,10]
+    
+    var lightUpInterval = 180
+    
+    let sleepingViewController = SleepingViewController()
+    
+    @IBOutlet weak var lightSlider: UISlider!
+    
+    @IBOutlet weak var wakeTimePicker: UIDatePicker!
+    
+    @IBOutlet weak var intervalPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        intervalPicker.delegate = self
+        intervalPicker.dataSource = self
+        intervalPicker.selectRow(userDefaults.integer(forKey: "lightUpInterval") - 1, inComponent: 0, animated: false)
+    }
+    
+    @IBAction func timerStartButtonAction(_ sender: UIButton) {
+        userDefaults.set("\(wakeTimePicker.date.timeIntervalSinceNow)", forKey: "wakeTime")
+        userDefaults.synchronize()
+    }
+    
+
+    //MARK: - Light Manipulation
+    func toggleTorch(with lightValue: Float) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+
+                if lightValue == 0 {
+                    device.torchMode = .off
+                } else {
+                    do {
+                        try device.setTorchModeOn(level: lightValue)
+                    } catch {
+                        print("error toggling light")
+                    }
+                }
+
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
+
+    
+
+    
+    
+}
+
+//MARK: - Light up interval picker settings
+
+extension AlarmSetViewController:  UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return lightIntervals.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(lightIntervals[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // The next line is for debuging
+        lightUpInterval = lightIntervals[row]
+        lightUpInterval = lightIntervals[row] * 60
+        userDefaults.set("\(lightUpInterval)", forKey: "lightUpInterval")
+        userDefaults.synchronize()
+        print("Light up interval is \(lightUpInterval)")
+    }
+}
--- 光目覚まし/Controllers/SleepingViewController.swift
+++ 光目覚まし/Controllers/SleepingViewController.swift
@@ -7,12 +7,154 @@
 //
 
 import UIKit
+import AVFoundation
+import Foundation
+import AudioToolbox
 
-class SleepingViewController: UIViewController {
+class SleepingViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer:AVAudioPlayer!
+    
+    let userDefaults = UserDefaults.standard
+    
+    var lightUpInterval: Int?
+    
+    var timer: Timer!
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet var backGroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        UIApplication.shared.isIdleTimerDisabled = true
+        lightUpInterval = userDefaults.integer(forKey: "lightUpInterval")
+        print(lightUpInterval!)
+        prepareAudio()
+        sleepingTimer()
+        toggleBackLight(with: 0)
+        backGroundView.backgroundColor = UIColor.black
     }
 
+    @IBAction func stopButton(_ sender: UIButton) {
+        UIApplication.shared.isIdleTimerDisabled = false
+        timer.invalidate()
+        toggleTorch(with: 0)
+        toggleBackLight(with: 1.0)
+        print(UIScreen.main.brightness)
+        audioPlayer.stop()
+        print("stop")
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: - Timer
+    func lightUpTimer(){
+        print("entered to the light up timer")
+        var lightStrength: Float = 0
+        let numberOfIntervals = 10
+        var currentinterval = 0
+        timer = Timer.scheduledTimer(withTimeInterval: Double(lightUpInterval!), repeats: true) { (timer) in
+            print("light up timer fires")
+            currentinterval += 1
+            lightStrength = Float(currentinterval) / Float(numberOfIntervals)
+            self.toggleTorch(with: lightStrength)
+            self.toggleBackLight(with: lightStrength)
+            print(lightStrength)
+            if currentinterval >= 10 {
+                self.audioPlayer.play()
+                timer.invalidate()
+            }
+            self.toggleTorch(with: lightStrength)
+        }
+        if timer == nil {
+            print(timer)
+            return
+        }
+        print(1)
+//        print(AlermSetViewController().setTime())
+    }
+    
+    //MARK: - Start the light up timer
+    func sleepingTimer(){
+        let sleepInterval = calculateInterval()
+        print("start")
+        timer = Timer.scheduledTimer(withTimeInterval: sleepInterval, repeats: false) { (Timer) in
+        print("end")
+            self.backGroundView.backgroundColor = UIColor.white
+            self.lightUpTimer()
+        }
+    }
+    
+    //MARK: - Caluculate sleep Interval
+    func calculateInterval() -> Double {
+        var interval = userDefaults.double(forKey: "wakeTime")
+        if interval < 0 {
+            interval = 864000 + interval
+        }
+        interval = interval - Double(lightUpInterval!) * 10
+//        if interval < 0 {
+//            self.dismiss(animated: true, completion: nil)
+//        }
+        print("Sleep for \(interval) seconds")
+        return interval
+    }
+    
+    //MARK: - Light Manipulation
+    func toggleTorch(with lightValue: Float) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                
+                if lightValue == 0 {
+                    device.torchMode = .off
+                } else {
+                    do {
+                        try device.setTorchModeOn(level: lightValue)
+                    } catch {
+                        print("error toggling light")
+                    }
+                }
+                
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+        
+    }
+    
+    func toggleBackLight(with lightValue: Float){
+        UIScreen.main.brightness = CGFloat(lightValue)
+    }
+}
+
+
+
+extension SleepingViewController {
+    func prepareAudio(){
+        // 再生する audio ファイルのパスを取得
+        let audioPath = Bundle.main.path(forResource: "1", ofType:"mp3")!
+        let audioUrl = URL(fileURLWithPath: audioPath)
+        
+        
+        // auido を再生するプレイヤーを作成する
+        var audioError:NSError?
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioUrl)
+        } catch let error as NSError {
+            audioError = error
+            audioPlayer = nil
+        }
+        
+        // エラーが起きたとき
+        if let error = audioError {
+            print("Error \(error.localizedDescription)")
+        }
+        
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+    }
 }
