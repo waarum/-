3a8a7322327c158332d4cb70dd318ea32689ee8d
Waarum <faiya.med@gmail.com>
2019-05-05 20:58:28 +0900

    set up the picker

--- 光目覚まし/Controllers/AlarmSetViewController.swift
+++ 光目覚まし/Controllers/AlarmSetViewController.swift
@@ -19,12 +19,12 @@ class AlarmSetViewController: UIViewController {
     
     var hours:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
     var minutes:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
+    let defaults = UserDefaults.standard
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        wakeUpTimePicker.delegate = self
-        wakeUpTimePicker.dataSource = self
+        setUpPicker()
     }
     
     @IBAction func timerStartButtonAction(_ sender: UIButton) {
@@ -61,6 +61,7 @@ class AlarmSetViewController: UIViewController {
 }
 
 extension AlarmSetViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    // set up the outlets
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
@@ -83,5 +84,24 @@ extension AlarmSetViewController: UIPickerViewDataSource, UIPickerViewDelegate {
         }
     }
     
+    // How it works
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            defaults.set(hours[row], forKey: Keys.hour)
+        default:
+            defaults.set(minutes[row], forKey: Keys.minute)
+        }
+    }
     
+    // Set up in launching
+    func setUpPicker() {
+        wakeUpTimePicker.delegate = self
+        wakeUpTimePicker.dataSource = self
+        defaults.register(defaults: [Keys.hour: 7, Keys.minute: 0])
+        let hour = defaults.integer(forKey: Keys.hour)
+        let minute = defaults.integer(forKey: Keys.minute)
+        wakeUpTimePicker.selectRow(hour, inComponent: 0, animated: false)
+        wakeUpTimePicker.selectRow(minute, inComponent: 1, animated: false)
+    }
 }
