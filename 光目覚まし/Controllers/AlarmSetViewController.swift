593eb38f98eaf679b20a16a38b8b987686c5d729
Waarum <faiya.med@gmail.com>
2019-05-06 12:42:09 +0900

    restore segue

--- 光目覚まし/Controllers/AlarmSetViewController.swift
+++ 光目覚まし/Controllers/AlarmSetViewController.swift
@@ -20,46 +20,12 @@ class AlarmSetViewController: UIViewController {
     var hours:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
     var minutes:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
     let defaults = UserDefaults.standard
-
-    
-    var time = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpPicker()
         }
-    }
 
-
-    
-    
-
-    //MARK: - Light Manipulation
-    func toggleTorch(with lightValue: Float) {
-        guard let device = AVCaptureDevice.default(for: .video) else { return }
-
-        if device.hasTorch {
-            do {
-                try device.lockForConfiguration()
-
-                if lightValue == 0 {
-                    device.torchMode = .off
-                } else {
-                    do {
-                        try device.setTorchModeOn(level: lightValue)
-                    } catch {
-                        print("error toggling light")
-                    }
-                }
-
-                device.unlockForConfiguration()
-            } catch {
-                print("Torch could not be used")
-            }
-        } else {
-            print("Torch is not available")
-        }
-    
-    
 }
 
 extension AlarmSetViewController: UIPickerViewDataSource, UIPickerViewDelegate {
